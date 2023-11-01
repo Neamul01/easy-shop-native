9d6addd08d79a5444e9a051bcb75651e0a0472ac
Neamul01 <neamulhaque2002@gmail.com>
2023-11-01 23:59:58 +0600

    redux added

--- App.js
+++ App.js
@@ -4,15 +4,19 @@ import Header from "./Shared/Header";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import Main from "./Navigators/Main";
+import { Provider } from "react-redux";
+import store from "./Redux/store";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.container}>
-        <Header />
-        <Main />
-      </SafeAreaView>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <Header />
+          <Main />
+        </SafeAreaView>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
