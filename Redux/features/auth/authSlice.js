f7e495f68f4a448c91d3819e387405508a2f4ea7
Neamul01 <neamulhaque2002@gmail.com>
2023-11-13 00:43:32 +0600

    auth logics added

--- Redux/features/auth/authSlice.js
+++ Redux/features/auth/authSlice.js
@@ -5,6 +5,7 @@ const authSlice = createSlice({
   name: "authSlice",
   initialState: {
     user: null,
+    isAdmin: false,
   },
   reducers: {
     logOut: async () => {
@@ -13,14 +14,39 @@ const authSlice = createSlice({
     },
     setUser: (state, action) => {
       state.user = action.payload;
-      console.log("inside set user", state.user);
+      state.isAdmin = state.user.isAdmin;
+      // console.log("inside set user", state.isAdmin);
     },
-    getUser: async (state) => {
-      return state.user;
+    getUserFromStorage: async (state) => {
+      try {
+        const jsonUser = await AsyncStorage.getItem("user");
+        const user = jsonUser != null ? JSON.parse(jsonUser) : null;
+        state.user = user;
+        state.isAdmin = state.user.isAdmin;
+        // console.log(user);
+        return user;
+      } catch (error) {
+        console.error("Error while getting user from AsyncStorage:", error);
+        return null;
+      }
+    },
+    userIsAdmin: (state) => {
+      return state.isAdmin;
+    },
+    isAuthenticated: async (state) => {
+      const token = await AsyncStorage.getItem("token");
+      return token ? true : false;
     },
   },
 });
 
 export const selectUser = (state) => state.authSlice.user;
-export const { logOut, getUser, setUser } = authSlice.actions;
+export const selectIsAdmin = (state) => state.authSlice.isAdmin;
+export const {
+  logOut,
+  setUser,
+  getUserFromStorage,
+  userIsAdmin,
+  isAuthenticated,
+} = authSlice.actions;
 export default authSlice.reducer;
