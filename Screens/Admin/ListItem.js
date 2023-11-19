80eb1308d8ebc707668753f8a23638429e5c62f2
Neamul01 <neamulhaque2002@gmail.com>
2023-11-20 01:36:40 +0600

    modal added

--- Screens/Admin/ListItem.js
+++ Screens/Admin/ListItem.js
@@ -1,20 +1,70 @@
 import {
   Dimensions,
   Image,
+  Modal,
   StyleSheet,
   Text,
+  TouchableHighlight,
   TouchableOpacity,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import { Button } from "@rneui/themed";
 
 const { width } = Dimensions.get("window");
 
 export default function ListItem(props) {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const navigation = useNavigation();
+
   return (
     <View>
+      <Modal
+        animationType="fade"
+        transparent
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modelView}>
+            <TouchableOpacity
+              underlayColor={"#E8E8E8E8"}
+              onPress={() => setModalVisible(false)}
+              style={{
+                alignSelf: "flex-end",
+                position: "absolute",
+                top: 5,
+                right: 10,
+              }}
+            >
+              <Icon name="close" size={20} />
+            </TouchableOpacity>
+            <Button
+              title="Edit"
+              onPress={() => [
+                navigation.navigate("ProductForm"),
+                setModalVisible(false),
+              ]}
+              containerStyle={styles.modalButton}
+            />
+            <Button
+              containerStyle={styles.modalButton}
+              title="Delete"
+              onPress={() => [
+                // Delete Here
+              ]}
+            />
+          </View>
+        </View>
+      </Modal>
       <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("ProductDetail", { item: props });
+        }}
+        onLongPress={() => setModalVisible(true)}
         style={[
           styles.container,
           {
@@ -60,5 +110,36 @@ const styles = StyleSheet.create({
     flexWrap: "wrap",
     margin: 3,
     width: width / 6,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modelView: {
+    width: "40%",
+    height: "auto",
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 10,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    gap: 5,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 2.84,
+    elevation: 5,
+    // paddingLeft: 20,
+    paddingHorizontal: 20,
+    paddingTop: 40,
+    paddingBottom: 10,
+  },
+  modalButton: {
+    width: "100%",
   },
 });
